52fc36a15fb4b65dd880eb8e38bf2e204efea012
roastea <syugamiruku0210@gmail.com>
2024-08-21 15:47:12 +0900

    ライフカウントを追加しました

--- ColorWatch/Assets/Enemy/Enemy.cs
+++ ColorWatch/Assets/Enemy/Enemy.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Enemy : MonoBehaviour
 {
+    public TextMeshProUGUI lifeCount;
+    public int life;
+
+    private void Start()
+    {
+        life = 3;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             Debug.Log("touch!");
+            life--;
+            lifeCount.SetText("Life : {0}", life);
         }
     }
 }
