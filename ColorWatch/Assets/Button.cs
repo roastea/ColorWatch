12ac89b38a418d980a26b6d67269b1fa5092ca15
ayuzakana84 <ayunume@gmail.com>
2024-10-01 14:51:54 +0900

    タイトル画面、ゲームオーバー画面、クリア画面への遷移を出来るようにしました。

---
+++ ColorWatch/Assets/Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PushStartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PushBackTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
