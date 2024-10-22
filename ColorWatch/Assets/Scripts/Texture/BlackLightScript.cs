6ab17708dc64b7e9b89ed23f4a084e7726b6561a
ayuzakana84 <ayunume@gmail.com>
2024-10-22 15:35:11 +0900

    バッテリーゲージとスタミナゲージを追加しました

--- ColorWatch/Assets/Scripts/Texture/BlackLightScript.cs
+++ ColorWatch/Assets/Scripts/Texture/BlackLightScript.cs
@@ -1,12 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class BlackLightScript : MonoBehaviour
 {
     public Transform judge;
     public Transform player;
     public float maxAngle = 25f;
+
+    private bool powerOn = false;
+
+    [SerializeField] private float maxBattery = 100f; //バッテリーの最大値
+    private float nowbattery;
+
+    public GameObject batteryGauge;
+    private Slider batterySlider;
+
+    void Start()
+    {
+        batterySlider = batteryGauge.GetComponent<Slider>();
+        batterySlider.maxValue = maxBattery;
+        nowbattery = maxBattery;
+    }
+
+    public void OnLight(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (!powerOn)
+            {
+                powerOn = true;
+            }
+            else
+            {
+                powerOn = false;
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,11 +52,25 @@ public class BlackLightScript : MonoBehaviour
                 {
                     if(material.shader.name=="Unlit/BlackLight")
                     {
-                        material.SetVector("_Judge", new Vector4(judge.position.x, judge.position.y, judge.position.z, 0));
+                        if (powerOn) //ライトON
+                        {
+                            material.SetVector("_Judge", new Vector4(judge.position.x, judge.position.y, judge.position.z, 0));
 
-                        material.SetVector("_Position", new Vector4(player.position.x, player.position.y, player.position.z, 0));
+                            material.SetVector("_Position", new Vector4(player.position.x, player.position.y, player.position.z, 0));
 
-                        material.SetFloat("_MaxAngle", maxAngle);
+                            material.SetFloat("_MaxAngle", maxAngle);
+
+                            nowbattery -= 0.01f;
+                            batterySlider.value = nowbattery;
+                        }
+                        else //ライトOFF
+                        {
+                            material.SetVector("_Judge", new Vector4(0, 0, 0, 0));
+
+                            material.SetVector("_Position", new Vector4(0, 0, 0, 0));
+
+                            material.SetFloat("_MaxAngle", 0);
+                        }
                     }
                 }
             }
