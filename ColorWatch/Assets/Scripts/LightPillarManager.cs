231fa6336a31f98b3e4c99cabb0ca9293d58aed3
ayuzakana84 <ayunume@gmail.com>
2024-12-03 18:48:00 +0900

    倒した敵のカウントの追加

--- ColorWatch/Assets/Scripts/LightPillarManager.cs
+++ ColorWatch/Assets/Scripts/LightPillarManager.cs
@@ -42,6 +42,7 @@ public class LightPillarManager : MonoBehaviour
         }
     }
 
+    //Œõ‚Ì’Œ‚ÌˆÊ’u‚Ì•ÏX
     public void ChangePillarPoint(GameObject setPillar)
     {
         int index = 0;
