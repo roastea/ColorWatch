0c7bc34da22fb171f119a9fa9d38ab5448a5beae
roastea <syugamiruku0210@gmail.com>
2024-12-10 12:43:06 +0900

    青より先に赤を取り戻したときに床一面に赤が描画されてしまう問題を修正しました
    テレサの移動速度を調整しました(3→5)

--- ColorWatch/Assets/Scripts/EnemyShySearch.cs
+++ ColorWatch/Assets/Scripts/EnemyShySearch.cs
@@ -34,6 +34,8 @@ public class EnemyShySearch : MonoBehaviour
     {
         agent = enemyShy.GetComponent<NavMeshAgent>();
 
+        agent.speed = 5;
+
         //blacklightscript = blacklightobject.GetComponent<BlackLightScript>();
     }
 
@@ -59,7 +61,7 @@ public class EnemyShySearch : MonoBehaviour
         {
             playerLook = false;
             //Debug.Log("èoÇΩ");
-            agent.speed = 3;
+            agent.speed = 5;
         }
     }
 
@@ -71,7 +73,7 @@ public class EnemyShySearch : MonoBehaviour
         }
         else
         {
-            //agent.speed = 3;
+            //agent.speed = 5;
 
             if (other.CompareTag("Player")) //ok
             {
@@ -91,7 +93,7 @@ public class EnemyShySearch : MonoBehaviour
                         shyLook = true;
 
                         agent.destination = target;
-                        agent.speed = 3;
+                        agent.speed = 5;
                     }
                     else
                     {
